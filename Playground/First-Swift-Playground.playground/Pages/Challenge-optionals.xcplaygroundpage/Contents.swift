160bddecbb3cc6838b6ff46a61ca676fb2e36ea9
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-26 16:41:37 -0600

    Add challenges description

---
+++ Playground/First-Swift-Playground.playground/Pages/Challenge-optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,21 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Challenge - optionals
+ 
+ ## Challenge 1
+ You've been provided with a constat below, hasAllergies, which gas been set to true.
+ Below that, declare a Optional string variable named dogName.
+ On the next line use a ternary conditional operator to set the value of dogName to nil if hasAllergies is true and to "Mongo" otherwise.
+ */
+
+/*:
+
+## Challenge 2
+Create a constant called parsedInt and set it to Int("10"). Swift will attempt to parse the string 1- and convert it to an Int.
+ Place your mouse over the constant name and use Option-Click to check the type of parsedInt. Why is parsedInt an optional here?
+*/
+
+//: [Next](@next)
