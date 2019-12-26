d502f99732baa342e5a51ff2a0a616260c4b2e83
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-26 16:53:45 -0600

    challenges resolved

--- Playground/First-Swift-Playground.playground/Pages/Challenge-optionals.xcplaygroundpage/Contents.swift
+++ Playground/First-Swift-Playground.playground/Pages/Challenge-optionals.xcplaygroundpage/Contents.swift
@@ -13,11 +13,20 @@ import Foundation
 
 
 let hasAllergies = true
+var dogName: String? = hasAllergies ? nil : "Mongo
 /*:
 
 ## Challenge 2
 Create a constant called parsedInt and set it to Int("10"). Swift will attempt to parse the string 1- and convert it to an Int.
  Place your mouse over the constant name and use Option-Click to check the type of parsedInt. Why is parsedInt an optional here?
 */
+let parsedInt = Int("10")
+
+/*:
+ # Challenge 3
+ Create another constant named newPArsedInt and set it equal to Int("cat")
+ What will the value of `newParsedInt` be? Why
+ */
+let newParsedInt = Int("cat")
 
 //: [Next](@next)
