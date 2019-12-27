e822c52d2c30d44985a7c5ed401fc69be8bb2abb
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-26 20:00:23 -0600

    init operating on array

---
+++ Playground/Beginning-collections.playground/Pages/Operating-on-arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Operating on arrays
+ 
+ */
+
+var pastries: [String] = ["cookie", "danish", "cupcake", "donut", "pie", "brownie"]
+
+//: [Next](@next)
