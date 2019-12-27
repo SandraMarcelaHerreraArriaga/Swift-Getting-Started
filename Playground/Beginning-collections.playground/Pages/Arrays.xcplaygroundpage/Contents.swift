0c2005514aea4872afa9251b0f581deb1874ea7e
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-26 19:45:32 -0600

    Arrays

---
+++ Playground/Beginning-collections.playground/Pages/Arrays.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # Arrays
+ */
+//let pastries = ["cookie", "cupcake", "donut", "pie"]
+//let pastries [String] = ["cookie", "cupcake", "donut", "pie"]
+
+///To initialize an empty array
+var pastries [String] = []
+pastries.append("cookie")
+pastries.append("danish")
+pastries += ["cupcake", "donut", "pie", "brownie"]
+
+//: [Next](@next)
