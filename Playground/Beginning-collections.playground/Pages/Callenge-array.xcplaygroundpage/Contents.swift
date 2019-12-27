803f72600d61090cb4b03b4188ea46758cfcb8d8
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-27 14:38:52 -0600

    Challenges array description

---
+++ Playground/Beginning-collections.playground/Pages/Callenge-array.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+
+/*:
+ # Challenge-array
+ 
+ ## Challenge 1
+ Using the players array below, and the array methods and properties you learned about in this video, determine the following things about the array.
+ 
+ (a) The count of elements in the array (b) Whether or not the array contains the string value "Charles" (c) The first element in the array (d) The last element in the array
+ */
+
+var players = ["Alice", "Bob", "Dan", "Eli", "Frank"]
+
+
+/*:
+ ## Challenge 2
+ Some new players have joined the game: Charles, Gloria and Hermione.
+ You'd like to add them to the array; Gloria and Hermione at the end, and charles somewhere in the middle.
+ 
+ (a) Insert Charles at index 2 in the array. (b) Add Gloria and Hermione at the end of the array in a single line of code.
+ */
+
+
+
+/*:
+ ## Challenge 3
+  Create another new constant array named teamOne that consists of the last four members of the players array; that would be the range of elements from 4...7
+ */
+
+//: [Next](@next)
