2a77961234ee2080cdce933ca0a3614c6d50c734
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2019-12-27 16:18:19 -0600

    add while loops examples

---
+++ Playground/While-loops.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+/*:
+ # While loops
+ */
---
+++ Playground/While-loops.playground/Pages/While-loops-examples.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ # While loops
+ */
+
+var i = 1
+while i < 10 {
+    print(i)
+    i += 1
+}
+
+
+print("Counting up again")
+i = 1
+
+// do while in swift is repeat
+repeat{
+    print(i)
+    i += 1
+} while i < 10
+
+
+//: [Next](@next)
