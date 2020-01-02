12a73e2caaa3cb9a4270837ec283e91e38da365d
Sandra Marcela Herrera Arriaga <sandra.h.arriaga@gmail.com>
2020-01-02 11:20:52 -0600

    challenge iterating collections description

---
+++ Playground/iterating-collections.playground/Pages/iterating-collections-challenge.xcplaygroundpage/Contents.swift
@@ -0,0 +1,26 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*:
+ ## Challenge 1
+ I've provided an array of pastries below
+ Create a loop that iterates over each element of the array, and uses an if statement inside the loop to print out the pastries that start with the letter "c"
+ 
+ There's no need to use i or another index variable to loop through the array, use the more compact way to iterate over every element of an array.
+ To check if first letter of a string matches a certain character, you can use code to the following form:
+ 
+ if pastry[pastry.startIndex] == "c"
+ */
+
+
+
+/*:
+ ## Challenge 2
+ You can write an even more compact form to the loop above, using where clause as part of your for loop condition
+ Remove the if block inside your loop, the only statement inside your loop should be print(pastry)
+ Add a where clause to your for loop with the same if condition you used in the loop above.
+ your output should be the same as in challenge 1.
+
+ */
+//: [Next](@next)
